54933b38bd175d035ea5b174411e35254f213445
ajitbhaik <ajit.bhaik@figmd.in>
2019-04-10 12:11:25 +0530

    fs_demo

--- practice/fs_demo.js
+++ practice/fs_demo.js
@@ -26,3 +26,23 @@ fs.writeFile(
     );
   }
 );
+
+// Read file
+fs.readFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "utf-8",
+  (err, data) => {
+    if (err) throw err;
+    console.log("File opened --- ", data);
+  }
+);
+
+// Rename file
+fs.rename(
+  path.join(__dirname, "/test", "hello.txt"),
+  path.join(__dirname, "/test", "NewHello.txt"),
+  err => {
+    if (err) throw err;
+    console.log("File Renamed --- ");
+  }
+);
