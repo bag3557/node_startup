68326aa20e5fe0a1e202a673f4947c5c7d0aeaf1
ajitbhaik <ajit.bhaik@figmd.in>
2019-04-10 14:04:46 +0530

    os, events, url, logger

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const Person = require('./person');
+// const Person = require('./person');
 
-// console.log(person.name + " --- " + person.age);
+// // console.log(person.name + " --- " + person.age);
 
-const personObj = new Person('Ajit Bhaik', 26);
+// const personObj = new Person('Ajit Bhaik', 26);
 
-personObj.greeting();
+// personObj.greeting();
 
+const Logger = require("./logger");
+
+const logger = new Logger();
+logger.on("message", data => console.log(`Called Listener: `, data));
+
+logger.log("Hello World");
+logger.log("Hi");
+logger.log("Hello");
---
+++ practice/url_demo.js
@@ -0,0 +1,31 @@
+const url = require("url");
+
+const myUrl = new URL(
+  "http://mywebsite.com:8000/hello.html?is=100&status=active"
+);
+
+// serialized URL
+console.log(myUrl.href);
+console.log(myUrl.toString());
+
+// Host (root domain)
+console.log(myUrl.host);
+
+// Hostname (does not get port)
+console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialized query
+console.log(myUrl.search);
+
+// Params object
+console.log(myUrl.searchParams);
+
+// Add params
+myUrl.searchParams.append("abc", "1234");
+console.log(myUrl.searchParams);
+
+// Loop through params
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`));
